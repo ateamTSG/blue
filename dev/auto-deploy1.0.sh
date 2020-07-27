729936125a781cd6ed0c0460dd49ecf9c8189ea9
root <root@L1001102762.ness.com>
2020-07-27 11:01:04 +0300

    copying from test to dev folder

---
+++ dev/auto-deploy1.0.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+OC=$(which oc)
+PROJ='blue-seal-project'
+
+## Creating ImageStream
+$OC create -n $PROJ -f imagestream-RC.yaml
+
+
+# Deploying DB
+## Creating Deployment Config
+$OC create -n $PROJ -f deployment-db.yaml
+
+$OC get pods -o name -l app=mongo | grep pod
+if [ $? -ne 0 ]
+then
+	echo "Creating Pod"
+	sleep 5
+fi
+# Getting the pod
+DB_POD=`$OC get pods -o name -l app=mongo | awk -F'/' '{print $2}'`
+# Waiting for container to Run
+touch /tmp/db.lock
+echo "Please wait while container starting..."
+while [ -f /tmp/db.lock ]
+do
+          sleep 7
+	  $OC get pods -l app=mongo | grep -w Running
+          if [ $? -eq 0 ]
+          then
+                rm -f /tmp/db.lock
+          fi
+done
+# Initializing MongoDB
+$OC rsh -n $PROJ $DB_POD mongo --eval "printjson(rs.initiate());printjson(cfg = rs.conf());printjson(cfg.members[0].host = \"$DB_POD:27017\");printjson(rs.reconfig(cfg, {force:true}))"
+
+# Deploying APP
+$OC create -n $PROJ -f deployment-app.yaml
+
+$OC get pods -o name -l app=rc | grep pod
+if [ $? -ne 0 ]
+then
+        echo "Creating Pod"
+        sleep 5
+fi
+
+# Getting the pod
+# Waiting for container to Run
+touch /tmp/app.lock
+echo "Please wait while container starting..."
+while [ -f /tmp/app.lock ]
+do
+          sleep 7
+          $OC get pods -l app=rc | grep -w Running
+          if [ $? -eq 0 ]
+          then
+                rm -f /tmp/app.lock
+          fi
+done
+# Exposing the RC route URL
+$OC expose -n $PROJ svc/app
+echo "-------------------------------------------------------"
+echo "Connect to URL: `$OC get -n $PROJ route | awk 'NR==2{print $2}'`"
+echo "-------------------------------------------------------"
+
---
+++ dev/old3.40-cmd.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#			    #
+# Blue POC		    #
+# TzahiA & LiorH 23/07/2020 #
+#			    #
+#############################
+
+rm -f '/tmp/1.lock'
+OC=$(which oc)
+CR='bluewhalecr.azurecr.io'
+PROJ='rocketchat-example'
+
+# Start mongo DB
+$OC new-app -n $PROJ --name=db $CR/mongo_custom:latest -l app=mongo
+sleep 5
+
+# Update PV 
+$OC set volume -n $PROJ "dc/db" --add --overwrite --name='db-volume-2' --type='persistentVolumeClaim' --claim-name='mongodb-claim2' --mount-path='/data/db'
+$OC set volume -n $PROJ "dc/db" --add --overwrite --name='db-volume-1' --type='persistentVolumeClaim' --claim-name='mongodb-claim' --mount-path='/data/configdb'
+sleep 2
+
+# Delete old pod
+OLD_POD=`$OC get -n $PROJ pods -o custom-columns='POD:.metadata.name' --no-headers -l app=mongo| head -1`
+$OC delete -n $PROJ pod/$OLD_POD
+
+## 
+touch /tmp/1.lock
+while [ -f /tmp/1.lock ]
+do
+	  sleep 10 
+	  $OC get pods | grep db | grep ContainerCreating
+	  if [ $? -ne 0 ]
+	  then
+		rm -f /tmp/1.lock
+	  fi
+done
+
+# Initiating ReplicaSet
+DB_POD=`$OC get -n $PROJ pods -o custom-columns='POD:.metadata.name' --no-headers -l app=mongo | tail -1`
+$OC exec -n $PROJ $DB_POD -i -t -- mongo --eval 'printjson(rs.initiate())'
+$OC rsh -n $PROJ $DB_POD mongo --eval "printjson(cfg = rs.conf());printjson(cfg.members[0].host = \"$DB_POD:27017\");printjson(rs.reconfig(cfg, {force:true}))"
+sleep 5
+
+# Start RocketChat App
+#$OC new-app -n $PROJ --name=app $CR/rocketchat:latest -l app=rc -e ROOT_URL=http://localhost -e MONGO_OPLOG_URL=mongodb://db:27017/local
+$OC new-app -n $PROJ --name=app $CR/rocketchat:latest -l app=rc -e ROOT_URL='http://localhost' -e MONGO_OPLOG_URL='mongodb://db:27017/local' -e MONGO_URL='mongodb://db/rocketchatdb'
+
+# Exposing the RC route URL
+$OC expose -n $PROJ svc/app
+sleep 5
+echo "-------------------------------------------------------"
+echo "Connect to URL: `$OC get -n $PROJ route | awk 'NR==2{print $2}'`"
+echo "-------------------------------------------------------"
+
+
+# For full CD process:
+# $OC set triggers dc/<name> --auto
+# Scale the App to 3
+#$OC scale dc/app --replicas=3
