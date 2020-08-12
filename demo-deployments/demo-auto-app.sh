f48321b8177823e234d127ee255da98c14d85007
tzahi <tzahi.ariel@tsgitsystems.com>
2020-08-12 18:55:41 +0300

    Finishing touches ;)

--- demo-deployments/demo-auto-app.sh
+++ demo-deployments/demo-auto-app.sh
@@ -2,16 +2,15 @@
 
 ## Setting defaults for Maintainence
 
-## keep 3 last completed deploy
-oc adm prune deployments --all --keep-complete=3 --keep-failed=0 --confirm
+## keep 3 last completed deployment and 1 failed for rollback
+oc adm prune deployments --all --keep-complete=3 --keep-failed=1 --confirm
 
 # Creating resources:
 oc apply -f demo-proj.yaml    ## Creating Project
-oc apply -f demo-rq.yaml      ## Project Quota
-oc apply -f demo-limits.yaml  ## Project limits
+oc apply -f demo-rq.yaml      ## Project ResourceQuota
+# oc apply -f demo-limits.yaml  ## Project limits (Use RQ instead)
 oc apply -f azurecr-secret.yaml ## Connecting to azure container registry
 oc apply -f demo-svc.yaml  ## adding the Service for 3000 port nodejs
 oc apply -f demo-is.yaml  ## Creating ImageStream for the container registry
 oc apply -f demo-dc2.yaml  ## Project Deployment Config with container limits
-oc apply -f demo-hpa.yaml ## Horizenal pod autoscaler Conf 3~6
-
+oc apply -f demo-hpa.yaml ## Horizinal pod autoscaler Conf 1~6
